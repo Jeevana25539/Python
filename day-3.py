2319ba22bea0aa7ff89cbbbfc549f9aae820b0e8
Jeevana25539 <swethasekher3@gmail.com>
2026-02-15 21:09:25 +0530

    day-3

---
+++ day-3.py
@@ -0,0 +1,46 @@
+"""#Finding the biggest number among the two numbers
+num1 = int(input())
+num2 = int(input())
+if num1>num2:
+    print("The biggest number is :",num1)
+else:
+    print("The biggest number is :",num2)"""
+
+
+"""#Find whether the number is even or odd
+
+num = int(input())
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")"""
+
+
+"""#Find the number is divisible by 5 or not
+
+num = int(input())
+if num % 5 == 0:
+    print("The number is divisible with 5")
+else:
+    print("The number is not divisible by 5")"""
+
+
+"""# Check the number is divisible by both 5 and 3
+
+num = int(input())
+if num % 5 == 0 and num % 3 == 0:
+    print("The is divible with both 5 and 3")
+else :
+    print("The number is not divisible with both 5 and 3")"""
+
+
+#check the number is divisible with anyone number of 4 or 5 nor not
+
+num = int(input())
+if num % 4 == 0 or num % 5 == 0 :
+    print("The number is divisible with  anyone  number 4 or 5")
+else :
+    print("The number is not divisible with anyone number 4 or 5")
+    
+
+    
